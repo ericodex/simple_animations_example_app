5d6b772fd74d17bdcb1b1c868652bf5f1072b512
felixblaschke <felix@felix-blaschke.de>
2019-04-07 16:51:05 +0200

    added switch-like checkbox demo (#1)

--- lib/examples_configuration.dart
+++ lib/examples_configuration.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:simple_animations_example_app/examples/rainbow_circle.dart';
+import 'package:simple_animations_example_app/examples/switchlike-checkbox.dart';
 import 'package:simple_animations_example_app/examples/typewriter_box.dart';
 
 ExamplesConfiguration getExamplesConfiguration() {
@@ -9,6 +10,9 @@ ExamplesConfiguration getExamplesConfiguration() {
       name: "Typewriter Box", builder: (context) => TypewriterBoxDemo()));
   configuration.add(ExampleItem(
       name: "Rainbow Circle", builder: (context) => RainbowCircleDemo()));
+  configuration.add(ExampleItem(
+      name: "Switch-like Checkbox",
+      builder: (context) => SwitchlikeCheckboxDemo()));
 
   return configuration;
 }
